4b5f52c8c168f581f6f44d2888e7fa293ab7a0a7
Sabrina <sabrinamandla17@gmail.com>
2025-03-21 00:37:52 -0700

    added basic calendar date listener

--- app/src/main/java/com/example/project_3245/MainActivity.java
+++ app/src/main/java/com/example/project_3245/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.project_3245;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener((calendarView1, year, month, day) -> {
+            String date = day + "/" + month + "/" + year;
+            Toast.makeText(MainActivity.this, date, Toast.LENGTH_SHORT).show();
+            System.out.println(date);
+        });
     }
 }
